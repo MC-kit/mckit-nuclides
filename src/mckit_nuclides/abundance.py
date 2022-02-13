f1797547cefbb5b5998d1d86053a355cfe13839e
dvp <dmitri_portnov@yahoo.com>
2022-02-13 12:44:50 +0300

    style: fixing ling/mypy warnings in abundance

--- src/mckit_nuclides/abundance.py
+++ src/mckit_nuclides/abundance.py
@@ -1,9 +1,19 @@
-"""Methods to convert fractions from mass to atoms adn from averaged by mass number to natural."""
+"""Methods to change nuclide abundance in compositions."""
 
 import pandas as pd
 
 from mckit_nuclides.nuclides import NUCLIDES_TABLE
 
 
-def convert_to_atomic_fraction(df: pd.DataFrame) -> None:
+def convert_to_atomic_fraction(df: pd.DataFrame) -> pd.DataFrame:
+    """Change fractions by mass to fractions by atoms.
+
+    Args:
+        df: Pandas DataFrame with having column "fractions" and
+            indexed by MultipleIndex (atomic_number, mass_number)
+
+    Returns:
+        DataFrame: df with modified column "fractions"
+    """
     df["fraction"] /= NUCLIDES_TABLE.loc[df.index]["relative_atomic_mass"]
+    return df
