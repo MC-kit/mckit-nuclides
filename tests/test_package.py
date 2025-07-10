b6b54593c216343c28893c5d48b832042fecaa1c
dvp <d.portnov@iterrf.ru>
2025-07-10 16:22:09 +0300

    dependencies: stop support Python 3.10
    This correspondes to numpy set of supported Python versions

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,20 +1,17 @@
+"""Test if installed package is of the current version."""
+
 from __future__ import annotations
 
 import re
-import sys
+import tomllib
 
 from pathlib import Path
 from re import sub as substitute
 
-if sys.version_info >= (3, 11):  # noqa: UP036, while we support python-3.10
-    import tomllib
-else:
-    import tomli as tomllib
-
 from mckit_nuclides import __version__
 
 
-def find_version_from_project_toml() -> str:
+def _find_version_from_project_toml() -> str:
     toml_path = Path(__file__).parent.parent / "pyproject.toml"
     assert toml_path.exists()
     pyproject = tomllib.loads(toml_path.read_text())
@@ -24,11 +21,11 @@ def find_version_from_project_toml() -> str:
 _VERSION_NORM_PATTERN = re.compile(r"-(?P<letter>.)[^.]*\.(?P<prepatch>.*)$")
 
 
-def normalize_version(version: str) -> str:
+def _normalize_version(version: str) -> str:
     return substitute(_VERSION_NORM_PATTERN, r"\1\2", version)
 
 
 def test_package() -> None:
     """This test checks if only current version is installed in working environment."""
-    version = find_version_from_project_toml()
-    assert __version__ == normalize_version(version), "Run 'uv install'"
+    version = _find_version_from_project_toml()
+    assert __version__ == _normalize_version(version), "Run 'uv install'"
